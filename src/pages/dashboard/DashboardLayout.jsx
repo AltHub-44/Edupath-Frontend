44633bc973393b7b90530325cbc9ecf56ad1294c
Samuel Blessed Woniowei <xcellentsam@gmail.com>
2025-03-19 11:19:30 +0100

    Fix netlify build fail

--- src/pages/dashboard/DashboardLayout.jsx
+++ src/pages/dashboard/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
+import Header from "./components/header";
 import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
